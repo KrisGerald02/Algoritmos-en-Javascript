c563d9bd373d309fa92da22731f15e440d046f6e
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-08-07 23:18:39 -0600

    Update Clase_10.js

--- ADA ITW/Clase_10.js
+++ ADA ITW/Clase_10.js
@@ -2,11 +2,11 @@
 Ejercicio 1
 Declará las variables vehiculo y litrosConsumidos. A continuación, realizá los cálculos para obtener el total a pagar, teniendo en cuenta las
 siguientes consideraciones:
-✓ Si el vehículo es “coche”, el precio por litro es de $86.
-✓ Si el vehículo es “moto”, el precio por litro es de $70.
-✓ Si el vehículo es “autobús”, el precio por litro es de $55.
-✓ Si los litros consumidos están entre 0 y 25, se ha de añadir $50 al total a pagar.
-✓ Si los litros consumidos son mayores a 25, se ha de añadir $25 al total a pagar.
+Si el vehículo es “coche”, el precio por litro es de $86.
+Si el vehículo es “moto”, el precio por litro es de $70.
+Si el vehículo es “autobús”, el precio por litro es de $55.
+Si los litros consumidos están entre 0 y 25, se ha de añadir $50 al total a pagar.
+Si los litros consumidos son mayores a 25, se ha de añadir $25 al total a pagar.
 Finalmente, imprimí en consola el total a pagar.
 */
 let vehiculo = "moto";
