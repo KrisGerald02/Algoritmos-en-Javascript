328e108f0e51c31ab8287f3227ea4b635422ac8b
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-08-07 23:28:57 -0600

    Update Clase_11.js

--- ADA ITW/Clase_11.js
+++ ADA ITW/Clase_11.js
@@ -53,3 +53,15 @@ let peso = parseFloat(promt('Ingrese su peso en kilogramos: '));
 function calculadorIMC(altura, peso) {
     return peso/(altura*altura)
 }
+
+//Ejercicio 6
+// Crear la función calculadorIMC() que reciba la altura en metros y el peso en kilogramos
+// y calcule el IMC de una persona. Luego, ejecutar la función
+// probando diferentes valores.
+
+function calculatorIMC(metros, peso) {
+  let cuenta = peso / (metros * metros);
+  return cuenta;
+}
+
+console.log("Su IMC es: " + calculatorIMC(1.57, 70));
