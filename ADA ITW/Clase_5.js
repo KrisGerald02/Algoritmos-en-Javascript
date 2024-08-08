152e64d0704cb89ebf35bd524434b75683ce70e6
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2024-08-07 22:04:00 -0600

    Update Clase_5.js

--- ADA ITW/Clase_5.js
+++ ADA ITW/Clase_5.js
@@ -1,1 +1,30 @@
+//Aprendiendo la Terminal
+const persona = {
+    nombre: "Geraldine",
+    estaEstudiando: true,
+    constantePI: 3.14,
+    constantePais: "Nicaragua",
+    cantidadLibros: 1,
+    tituloLibro: "Esquizofrenia",
+    esInteresante: true,
+    temasLibro: ['Mental', 'Salud'],
+    autorLibro: {
+        nombre: "Kazendra Varkalov",
+        nacionalidad: "Ruso"
+    },
+    esMayorEdad: true,
+    puedeVotar: true
+};
+
+console.log("Nombre: ", persona.nombre);
+console.log("Esta estudiando?: ", persona.estaEstudiando);
+console.log("Constante PI: ", persona.constantePI);
+console.log("Constante Pais: ", persona.constantePais);
+console.log("Cantidad de libros: ", persona.cantidadLibros);
+console.log("Titulo del libro: ", persona.tituloLibro);
+console.log("Es interesante?: ", persona.esInteresante);
+console.log("Temas del libro: ", persona.temasLibro);
+console.log("Autor del libro: ", persona.autorLibro);
+console.log("Es mayor de edad?: ", persona.esMayorEdad);
+console.log("Puede votar?: ", persona.puedeVotar);
 
